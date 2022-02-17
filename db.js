7cba1d5d24cecf29876a4c65eb34f0836c778ae7
AlicePihtjoe <alice.pihtjoe@khk.ee>
2022-02-17 10:50:55 +0200

    used DELETE statement

--- db.js
+++ db.js
@@ -9,8 +9,9 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers ORDER BY name DESC", function (err, result) {
+    var sql = "DELETE FROM customers WHERE address = 'Sky st 3'";
+    con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log(result);
+        console.log("Number of records deleted: " + result.affectedRows);
     });
 });
