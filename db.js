8c875ee7133c631c243e7d9aa51b2a11e7affb0f
AlicePihtjoe <alice.pihtjoe@khk.ee>
2022-02-17 10:55:02 +0200

    used UPDATE statement

--- db.js
+++ db.js
@@ -9,9 +9,9 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    var sql = "DELETE FROM customers WHERE address = 'Sky st 3'";
+    var sql = "UPDATE customers SET address = 'Läti' WHERE address = 'Highway 71'";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("Number of records deleted: " + result.affectedRows);
+        console.log(result.affectedRows + " record(s) updated");
     });
 });
