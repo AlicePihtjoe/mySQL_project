c5075149d86e6f99ae6fe184f837a82afdb2cf4a
AlicePihtjoe <alice.pihtjoe@khk.ee>
2022-02-17 11:20:07 +0200

    used LEFT JOIN statement

--- db.js
+++ db.js
@@ -9,9 +9,10 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    var sql = "SELECT * FROM customers LIMIT 3";
-    con.query(sql, function (err, result) {
+    console.log("Connected!");
+        var sql = "SELECT customers.name AS customer, products.name AS product FROM customers LEFT JOIN products ON customers.name = products.name";
+        con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log(result);
+    console.log(result);
     });
 });
