eb4d022b9ee28b824cc125f8fc299db022c7fc00
AlicePihtjoe <alice.pihtjoe@khk.ee>
2022-02-17 10:30:55 +0200

    Number of records inserted into customers table

--- db.js
+++ db.js
@@ -10,9 +10,16 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-    con.query(sql, function (err, result) {
+    var sql = "INSERT INTO customers (name, address) VALUES ?";
+    var values = [
+        ['John', 'Highway 71'],
+        ['Peter', 'Lowstreet 123'],
+        ['Mary', 'Sky st 3'],
+        ['George', 'The White House'],
+        ['William', 'Yellowstone']
+    ];
+    con.query(sql, [values], function (err, result) {
         if (err) throw err;
-        console.log("1 record inserted");
+        console.log("Number of records inserted: " + result.affectedRows);
     });
 });
